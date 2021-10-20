cece03349eac07d281de628b2c0fe3c99c895bab
Jonathan Wu <jwu20@stuy.edu>
2021-10-19 22:02:50 -0400

    updated k15 to kevin's code

--- 15_session/app.py
+++ 15_session/app.py
@@ -1,4 +1,4 @@
-# Team The Best Laptop - Jonathan W. [Loki] , Alif A. [The Eagle in the Sand], Kevin C. [Pipi] | Period 2
+# Team The Best Laptop - Jonathan Wu [Loki] , Alif Abdullah [The Eagle in the Sand], Kevin Cao [Pipi] | Period 2
 # SoftDev
 # K15 - Sessions Greetings/More form handling in HTML in flask app/We worked on creating a flask app that uses session capabilities to allow a user to login and logout.
 # 2021-10-18
@@ -6,11 +6,14 @@
 from flask import Flask             # facilitate flask webserving
 from flask import render_template   # facilitate jinja templating
 from flask import request           # facilitate form submission
+from flask import session           # facilitate session handling
 app = Flask(__name__)
+
+app.secret_key = "professional_secret_key"
 
 
 @app.route("/")  # , methods=['GET', 'POST'])
-def disp_loginpage():
+def disp_login():
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
     print(app)
@@ -20,8 +23,13 @@ def disp_loginpage():
     print(request.args)
     print("***DIAG: request.headers ***")
     print(request.headers)
-    landingText = "Enter your username below to proceed."
-    return render_template('login.html', message = landingText)
+    if 'user' in session:  # checks to see if there is a session
+        user = session['user']
+        loadText = "Click the button below to logout."
+        return render_template("response.html", username=user, request_method=request.method, message=loadText)
+    else:  # return login page
+        landingText = "Enter your username below to proceed."
+        return render_template('login.html', message=landingText)
 
 
 @app.route("/auth")
@@ -35,34 +43,43 @@ def response():
     print(request.args)
     print("***DIAG: request.headers ***")
     print(request.headers)
-    user0 = 'kevinjonleafy'
-    passw0 = 'dellthebestlaptop15'
+    user0 = 'a'
+    passw0 = 'b'
     loadText = ""
     user = ""
     passw = ""
-    if request.method == 'GET':
-        user = request.args['username']
-        passw = request.args['password']
-    elif request.method == 'POST':
-        user = request.form['username']
-        passw = request.form['password']
-    if user0 != user and passw0 != passw:
-        loadText = "Could not log in. User and password wrong."
-        return render_template("login.html", message = loadText)
-    elif user0 != user and passw0 == passw:
-        loadText = "Could not log in. User is wrong."
-        return render_template("login.html", message = loadText)
-    elif user0 == user and passw0 != passw:
-        loadText = "Could not log in. Password is wrong."
-        return render_template("login.html", message = loadText)
-    else:
+    if 'user' in session:  # checks to see if there is a session, if there is show response page
+        user = session['user']
         loadText = "Click the button below to logout."
-        return render_template("response.html", username=user, password=passw, request_method = request.method, message = loadText)
+        return render_template("response.html", username=user, request_method=request.method, message=loadText)
+    else:  # check for any username/password errors
+        if request.method == 'GET':
+            user = request.args['username']
+            passw = request.args['password']
+        elif request.method == 'POST':
+            user = request.form['username']
+            passw = request.form['password']
+        if user0 != user and passw0 != passw:
+            loadText = "Could not log in. User and password wrong."
+            return render_template("login.html", message=loadText)
+        elif user0 != user and passw0 == passw:
+            loadText = "Could not log in. User is wrong."
+            return render_template("login.html", message=loadText)
+        elif user0 == user and passw0 != passw:
+            loadText = "Could not log in. Password is wrong."
+            return render_template("login.html", message=loadText)
+        else:  # if username and password match, show response page
+            session['user'] = user
+            loadText = "Click the button below to logout."
+            return render_template("response.html", username=user, request_method=request.method, message=loadText)
+
 
 @app.route("/logout")
 def logoutSession():
-    landingText = "You've logged out. Enter your username below to proceed. "
-    return render_template("login.html", message = landingText)
+    landingText = "You've logged out. Enter your login credentials below to proceed. "
+    session.pop('user', None)
+    return render_template("login.html", message=landingText)
+
 
 if __name__ == "__main__":
     app.debug = True
