c226c372b8ef0a31d1b7b0ac0c0376d941c2ff1c
Jonathan Wu <jwu20@stuy.edu>
2021-11-23 22:18:22 -0500

    uploaded k19 work

---
+++ 19_api/app.py
@@ -0,0 +1,46 @@
+# Crashing Waves: Jonathan Wu (Loki), Roshani Shrestha (Pete)
+# SoftDev pd0
+# K19: A RESTful Journey Skyward
+# 2021-11-24t
+# time spent:  hours
+
+from flask import Flask, render_template
+import json
+import ssl
+import urllib.request
+
+app = Flask(__name__) #create instance of class Flask
+
+@app.route("/")       #assign fxn to route
+def api():
+    '''
+        For those who get the ssl error involving certifcates being invalid,
+        this is atemporary solution that will bypass it until you resolve it on
+        your machine.
+    '''
+    # ssl._create_default_https_context = ssl._create_unverified_context
+    '''
+        This opens the key_nasa.txt file with the API key and then reads the
+        key inside of it.
+    '''
+    f = open("key_nasa.txt", "r")
+    api_key = f.read()
+    '''
+        Appends the API key to the url to access said API key for use later on
+        in the code.
+    '''
+    url = "https://api.nasa.gov/planetary/apod?api_key=" + api_key
+    '''
+        Opens the url, then reads the data requested from that url, decodes
+        it into a usable dictionary, and then we process the data given onto
+        our website.
+    '''
+    data = urllib.request.urlopen(url)
+    read_data = data.read()
+    d_data = read_data.decode('utf-8')
+    p_data = json.loads(d_data)
+    return render_template("main.html", pic = p_data['url'], comment = p_data['explanation'])
+
+if __name__ == "__main__":  # true if this file NOT imported
+    app.debug = True        # enable auto-reload upon code change
+    app.run()
