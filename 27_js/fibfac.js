affb4b7b6c6db2e4b53884a966ef5e664b0cfe57
Jonathan Wu <jwu20@stuy.edu>
2022-02-08 16:03:34 -0500

    semicolon fix

--- 27_js/fibfac.js
+++ 27_js/fibfac.js
@@ -8,18 +8,18 @@
 
 var fact = function(n) {
     if (n == 0) {
-        return 1
+        return 1;
     }
     else {
-        return n * fact(n - 1)
+        return n * fact(n - 1);
     }
-}
+};
 
 var fib = function(n) {
     if (n <= 1) {
-        return n
+        return n;
     }
     else {
-        return fib(n - 1) + fib(n - 2)
+        return fib(n - 1) + fib(n - 2);
     }
-}
+};
