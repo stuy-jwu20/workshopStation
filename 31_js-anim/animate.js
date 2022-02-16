c241c89aff95bba3394930225a1450360eb77930
Jonathan Wu <jwu20@stuy.edu>
2022-02-16 09:05:36 -0500

    update k31 work

--- 31_js-anim/animate.js
+++ 31_js-anim/animate.js
@@ -2,6 +2,7 @@
 // SoftDev pd2
 // K31 -- canvas based JS animation
 // 2022-02-15t
+// time spent: 45 minutes
 
 // model for HTML5 canvas-based animation
 
@@ -14,10 +15,10 @@ var dotButton = document.getElementById("buttonCircle"); // GET DOT BUTTON
 var stopButton = document.getElementById("buttonStop"); // GET STOP BUTTON
 
 //prepare to interact with canvas in 2D
-var ctx = c.getContext("2d"); // YOUR CODE HERE
+var ctx = c.getContext("2d"); 
 
 //set fill color to team color
-ctx.fillStyle = 'purple'; // YOUR CODE HERE
+ctx.fillStyle = 'purple'; 
 
 var requestID;  //init global var for use with animation frames
 
@@ -25,7 +26,7 @@ var requestID;  //init global var for use with animation frames
 //var clear = function(e) {
 var clear = (e) => {
   console.log("clear invoked...")
-  ctx.clearRect(0, 0, c.clientWidth, c.clientHeight);
+  ctx.clearRect(0, 0, c.clientWidth, c.clientHeight); //clears canvas
 };
 
 
@@ -38,27 +39,27 @@ var drawDot = () => {
   console.log("drawDot invoked...")
   clear();
   window.cancelAnimationFrame(requestID);
-  requestID = window.requestAnimationFrame(drawDot);
   ctx.beginPath();
   ctx.arc(250, 250, radius, 0, 2 * Math.PI);
   ctx.stroke();
   ctx.fill();
-  if (growing) {
-    if (radius === 250) {
+  if (growing) { //checks if the dot is growing - if so, the radius will increase; if not, it will decrease
+    if (radius >= 250) { //checks if the dot hits the sides of the canvas (or grows larger than that)
       growing = false;
     }
     else {
-      radius += 1;
+      radius += 1.75;
     }
   }
   else {
-    if (radius === 0) {
+    if (radius === 0) { //checks if the dot shrinks back to having a radius of 0
       growing = true;
     }
     else {
-      radius -= 1;
+      radius -= 1.75;
     }
   }
+  requestID = window.requestAnimationFrame(drawDot);
 };
 
 //var stopIt = function() {
